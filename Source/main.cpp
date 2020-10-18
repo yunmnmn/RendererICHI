13b7aa34bc91cb0134e4b6f6f8f6a5d3d0f1f06d
yunmnmn <yunyghu@gmail.com>
2020-10-18 23:12:34 +0900

    #EDIT: Update cmake and submodules

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,4 +1,11 @@
+#include <Memory/MemoryManager.h>
+#include <Memory/MemoryManagerInterface.h>
+
 int main()
 {
+   // Register the Memory Manager
+   Foundation::Memory::MemoryManager memoryManager;
+   Foundation::Memory::MemoryManagerInterface::Register(&memoryManager);
+
    return 0;
 }
