92e27221b7d12c1dc492c16eed571d8a388e1ef6
yunmnmn <yunyghu@gmail.com>
2020-10-19 00:14:57 +0900

    #EDIT: just testing

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,11 +1,15 @@
 #include <Memory/MemoryManager.h>
 #include <Memory/MemoryManagerInterface.h>
 
+#include <Util/HashName.h>
+
 int main()
 {
    // Register the Memory Manager
    Foundation::Memory::MemoryManager memoryManager;
    Foundation::Memory::MemoryManagerInterface::Register(&memoryManager);
+
+   Foundation::HashName test("temp");
 
    return 0;
 }
