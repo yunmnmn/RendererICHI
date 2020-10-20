2b04aacc9e8b602c32537f6fc4babee59565b331
yunmnmn <yunyghu@gmail.com>
2020-10-21 02:38:12 +0900

    #EDIT: update submodules

--- Source/main.cpp
+++ Source/main.cpp
@@ -2,14 +2,25 @@
 #include <Memory/MemoryManagerInterface.h>
 
 #include <Util/HashName.h>
+#include <Memory/ClassAllocator.h>
+
+struct ClassAllocatorTest
+{
+   CLASS_ALLOCATOR_PAGECOUNT_PAGESIZE(ClassAllocatorTest, 10u, 1024000);
+};
 
 int main()
 {
    // Register the Memory Manager
    Foundation::Memory::MemoryManager memoryManager;
    Foundation::Memory::MemoryManagerInterface::Register(&memoryManager);
+   size_t tmp = sizeof(ClassAllocatorTest);
+
+   ClassAllocatorTest* tester = new ClassAllocatorTest();
 
    Foundation::HashName test("temp");
+
+   delete tester;
 
    return 0;
 }
