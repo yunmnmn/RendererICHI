39dcb5750223892c16fe698d58f74e60849cdb09
yunmnmn <yunyghu@gmail.com>
2021-04-19 21:06:26 +0900

    #EDIT: Allow custom hash functor to be passed to the unordered_map

--- Include/std/unordered_map.h
+++ Include/std/unordered_map.h
@@ -8,7 +8,6 @@
 
 namespace Render
 {
-template <typename t_key, typename t_value>
-using unordered_map =
-    eastl::unordered_map<t_key, t_value, eastl::hash<t_key>, eastl::equal_to<t_key>, RendererEastlAllocator, false>;
+template <typename t_key, typename t_value, typename t_hash = eastl::hash<t_key>>
+using unordered_map = eastl::unordered_map<t_key, t_value, t_hash, eastl::equal_to<t_key>, RendererEastlAllocator, false>;
 }
